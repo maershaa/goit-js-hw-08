969fea38fec71592bda1e2b4b3b097b56967e26d
maershaa <129538294+maershaa@users.noreply.github.com>
2023-09-10 18:27:08 +0000

    Deploying to gh-pages from @ maershaa/goit-js-hw-08@ade340272be7bc36053e11df4fc9e306c8b91f1c 🚀

--- 03-feedback.6f55e5d3.js
+++ 03-feedback.6f55e5d3.js
@@ -1,4 +1,4 @@
-function e(e){return e&&e.__esModule?e.default:e}var t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},i="Expected a function",r=0/0,o=/^\s+|\s+$/g,a=/^[-+]0x[0-9a-f]+$/i,u=/^0b[01]+$/i,f=/^0o[0-7]+$/i,l=parseInt,c="object"==typeof t&&t&&t.Object===Object&&t,d="object"==typeof self&&self&&self.Object===Object&&self,s=c||d||Function("return this")(),m=Object.prototype.toString,p=Math.max,v=Math.min,g=function(){return s.Date.now()};/**
+function e(e){return e&&e.__esModule?e.default:e}var t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},n={},i="Expected a function",r=0/0,o=/^\s+|\s+$/g,a=/^[-+]0x[0-9a-f]+$/i,u=/^0b[01]+$/i,f=/^0o[0-7]+$/i,l=parseInt,c="object"==typeof t&&t&&t.Object===Object&&t,s="object"==typeof self&&self&&self.Object===Object&&self,d=c||s||Function("return this")(),m=Object.prototype.toString,p=Math.max,v=Math.min,g=function(){return d.Date.now()};/**
  * Checks if `value` is the
  * [language type](http://www.ecma-international.org/ecma-262/7.0/#sec-ecmascript-language-types)
  * of `Object`. (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
@@ -140,24 +140,24 @@ function e(e){return e&&e.__esModule?e.default:e}var t="undefined"!=typeof globa
  *
  * // Cancel the trailing debounced invocation.
  * jQuery(window).on('popstate', debounced.cancel);
- */function(e,t,n){var r,o,a,u,f,l,c=0,d=!1,s=!1,m=!0;if("function"!=typeof e)throw TypeError(i);function h(t){var n=r,i=o;return r=o=void 0,c=t,u=e.apply(i,n)}function S(e){var n=e-l,i=e-c;// Either this is the first call, activity has stopped and we're at the
+ */function(e,t,n){var r,o,a,u,f,l,c=0,s=!1,d=!1,m=!0;if("function"!=typeof e)throw TypeError(i);function h(t){var n=r,i=o;return r=o=void 0,c=t,u=e.apply(i,n)}function S(e){var n=e-l,i=e-c;// Either this is the first call, activity has stopped and we're at the
 // trailing edge, the system time has gone backwards and we're treating
 // it as the trailing edge, or we've hit the `maxWait` limit.
-return void 0===l||n>=t||n<0||s&&i>=a}function j(){var e,n,i,r=g();if(S(r))return O(r);// Restart the timer.
-f=setTimeout(j,(e=r-l,n=r-c,i=t-e,s?v(i,a-n):i))}function O(e){return(// Only invoke if we have `lastArgs` which means `func` has been
+return void 0===l||n>=t||n<0||d&&i>=a}function j(){var e,n,i,r=g();if(S(r))return O(r);// Restart the timer.
+f=setTimeout(j,(e=r-l,n=r-c,i=t-e,d?v(i,a-n):i))}function O(e){return(// Only invoke if we have `lastArgs` which means `func` has been
 // debounced at least once.
 (f=void 0,m&&r)?h(e):(r=o=void 0,u))}function T(){var e,n=g(),i=S(n);if(r=arguments,o=this,l=n,i){if(void 0===f)return(// Reset any `maxWait` timer.
 c=e=l,// Start the timer for the trailing edge.
-f=setTimeout(j,t),d?h(e):u);if(s)return(// Handle invocations in a tight loop.
-f=setTimeout(j,t),h(l))}return void 0===f&&(f=setTimeout(j,t)),u}return t=b(t)||0,y(n)&&(d=!!n.leading,a=(s="maxWait"in n)?p(b(n.maxWait)||0,t):a,m="trailing"in n?!!n.trailing:m),T.cancel=function(){void 0!==f&&clearTimeout(f),c=0,r=l=o=f=void 0},T.flush=function(){return void 0===f?u:O(g())},T}(e,t,{leading:r,maxWait:t,trailing:o})};// Определение константы STORAGE_KEY, которая будет использоваться в качестве ключа для сохранения и извлечения данных из локального хранилища.
+f=setTimeout(j,t),s?h(e):u);if(d)return(// Handle invocations in a tight loop.
+f=setTimeout(j,t),h(l))}return void 0===f&&(f=setTimeout(j,t)),u}return t=b(t)||0,y(n)&&(s=!!n.leading,a=(d="maxWait"in n)?p(b(n.maxWait)||0,t):a,m="trailing"in n?!!n.trailing:m),T.cancel=function(){void 0!==f&&clearTimeout(f),c=0,r=l=o=f=void 0},T.flush=function(){return void 0===f?u:O(g())},T}(e,t,{leading:r,maxWait:t,trailing:o})};// Определение константы STORAGE_KEY, которая будет использоваться в качестве ключа для сохранения и извлечения данных из локального хранилища.
 const h="feedback-data",S={form:document.querySelector(".feedback-form"),input:document.querySelector('[name="email"]'),textarea:document.querySelector('[name="message"]')},j={};// Функция saveDataToLocalStorage записывает данные из объекта formData в локальное хранилище с помощью localStorage.setItem.
 function O(){localStorage.setItem(h,JSON.stringify(j))}// Добавление обработчика события submit на форму. Когда форма отправляется, вызывается функция onFormSubmit.
 S.form.addEventListener("submit",// Внутри функции onFormSubmit сначала предотвращается стандартное поведение формы с помощью evt.preventDefault(). Затем проверяется, заполнены ли оба поля ввода (email и message). Если хотя бы одно из полей не заполнено, выводится предупреждающее сообщение с помощью alert и функция завершается с помощью return, чтобы остальные действия не выполнялись.
 function(e){// Здесь вы проверяете, заполнены ли оба поля email и message в форме. Если хотя бы одно из полей пустое, выводится alert с предупреждением.
 if(e.preventDefault(),""===S.input.value||""===S.textarea.value){alert("Please fill in all the fields!");return}// После этого выполняется сброс значений полей формы с помощью evt.currentTarget.reset(), и данные удаляются из локального хранилища с помощью localStorage.removeItem(STORAGE_KEY).
-e.currentTarget.reset(),localStorage.removeItem(h)}),S.input.addEventListener("input",/*@__PURE__*/e(n)(// Далее, определены функции onInput и onTextareaInput, которые вызываются при вводе данных в поля ввода email и message. Внутри этих функций значения записываются в объект formData, а затем вызывается функция saveDataToLocalStorage, чтобы сохранить обновленные данные в локальное хранилище.
+e.currentTarget.reset(),localStorage.removeItem(h),j.email="",j.message=""}),S.input.addEventListener("input",/*@__PURE__*/e(n)(// Далее, определены функции onInput и onTextareaInput, которые вызываются при вводе данных в поля ввода email и message. Внутри этих функций значения записываются в объект formData, а затем вызывается функция saveDataToLocalStorage, чтобы сохранить обновленные данные в локальное хранилище.
 function(e){let t=e.target.getAttribute("name");j[t]=e.target.value,O()},500)),S.textarea.addEventListener("input",/*@__PURE__*/e(n)(function(e){let t=e.target.getAttribute("name");j[t]=e.target.value,O()},500)),// функция populateFormFields заполнит объект formData и сохранит его в локальное хранилище.
 //Функция populateFormFields используется для восстановления данных из локального хранилища при загрузке страницы. Она получает сохраненные данные, разбирает их с помощью JSON.parse, затем обновляет значения соответствующих полей формы и объекта formData.
-function(){let e=localStorage.getItem(h);if(e){let t=JSON.parse(e);for(let e in t)if(t.hasOwnProperty(e)){let n=S.form.querySelector(`[name="${e}"]`);n&&(n.value=t[e]||"",j[e]=t[e])}}}();//# sourceMappingURL=03-feedback.e970da7a.js.map
+function(){let e=localStorage.getItem(h);if(e){let t=JSON.parse(e);for(let e in t)if(t.hasOwnProperty(e)){let n=S.form.querySelector(`[name="${e}"]`);n&&(n.value=t[e]||"",j[e]=t[e])}}}();//# sourceMappingURL=03-feedback.6f55e5d3.js.map
 
-//# sourceMappingURL=03-feedback.e970da7a.js.map
+//# sourceMappingURL=03-feedback.6f55e5d3.js.map
